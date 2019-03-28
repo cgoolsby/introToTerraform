1a02d2ddc15b067b473c036a59231edffeaa8f39
Curtis Goolsby <Curtis@Admins-MacBook-Pro.local>
2019-03-28 14:24:26 -0700

    fully functional spark autodeploy

--- stage6/modules/EC2/main.tf
+++ stage6/modules/EC2/main.tf
@@ -61,8 +61,71 @@ resource "aws_instance" "web" {
   provisioner "remote-exec" {
     inline = ["cat key.pub >> .ssh/authorized_keys"]
   }
+  provisioner "remote-exec" {
+    inline = ["chmod 0600 .ssh/id_rsa"]
+  }
+  provisioner "remote-exec" {
+    inline = ["wget http://www-us.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz"]
+  }
+  provisioner "remote-exec" {
+    inline = ["tar xvf spark-2.4.0-bin-hadoop2.7.tgz"]
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo mv spark-2.4.0-bin-hadoop2.7 /usr/local/spark"]
+  }
+  provisioner "file" {
+    source = "pathAppend.txt"
+    destination = "~/pathAppend.txt"
+  }
+  provisioner "remote-exec" {
+    inline = ["cat pathAppend.txt >> ~/.bashrc"]
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo echo \"\nmaster\nslave00\nslave01\nslave02\">>/usr/local/spark/conf/slaves"]
+  }
+  provisioner "remote-exec" {
+    inline = ["cp /usr/local/spark/conf/spark-env.sh.template /usr/local/spark/conf/spark-env.sh"]
+  }
 }
-
+resource "null_resource" "provision_master" {
+  connection {
+    user = "ubuntu"
+    host = "${element(aws_instance.web.*.public_ip,0)}"
+    private_key = "${file("~/.ssh/${var.aws_key_name}.pem")}"
+  }
+  
+  provisioner "remote-exec" {
+    inline = ["echo \"export SPARK_MASTER_HOST=${element(aws_instance.web.*.private_ip,0)}\" >> /usr/local/spark/conf/spark-env.sh"]
+  }
+  provisioner "remote-exec" {
+    inline = ["echo \"export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre\" >> /usr/local/spark/conf/spark-env.sh"]
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo echo \"${element(aws_instance.web.*.public_ip,0)} master\" | sudo tee --append /etc/hosts"]
+  }
+}
+resource "null_resource" "populate_slaves_and_host_etc" {
+  count = 3
+  connection {
+    user = "ubuntu"
+    host = "${element(aws_instance.web.*.public_ip,0)}"
+    private_key = "${file("~/.ssh/${var.aws_key_name}.pem")}"
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo echo \" ${element(aws_instance.web.*.public_ip,count.index+1)}  slave0${count.index}\" | sudo tee --append /etc/hosts"] 
+  }
+}
+resource "null_resource" "start_spark" {
+  depends_on = ["null_resource.provision_master", "null_resource.populate_slaves_and_host_etc"]
+  connection {
+    user = "ubuntu"
+    host = "${element(aws_instance.web.*.public_ip,0)}"
+    private_key = "${file("~/.ssh/${var.aws_key_name}.pem")}"
+  }
+  provisioner "remote-exec" {
+    inline = ["bash /usr/local/spark/sbin/start-all.sh"]
+  }
+}
 output "ips" {
   value = "${aws_instance.web.*.public_ip}"
 }
